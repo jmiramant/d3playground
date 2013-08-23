a88c7a9760842ef4e15c549e5bb568a4902c047e
jmiramant <josh@miramant.me>
2013-08-22 20:20:54 -0700

    updated seed db to render full company information

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ User.create({first_name: "Josh", last_name: "Miramant", architect_score: rand(10
 	User.create({first_name: "Dummy", last_name: "Account", architect_score: rand(100), location: 94108})
 	User.create({first_name: "Dummy", last_name: "Account", architect_score: rand(100), company_id: rand(30), location: 94108})
 end
-30.times do
+100.times do
 	Company.create({name: "Good.Co", size: rand(100), industry: "startup", architect_score: rand(100)})
 end
